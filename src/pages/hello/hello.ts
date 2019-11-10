1590d2279796a1cd47aa58314ac057bd4fe4ca96
Luiz Filipe Vieira <luiz.filipe.vieira@hotmail.com>
2019-11-09 21:34:32 -0300

    Criando Page hello

---
+++ src/pages/hello/hello.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the HelloPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-hello',
+  templateUrl: 'hello.html',
+})
+export class HelloPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    alert('Hello World');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HelloPage');
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HelloPage } from '../hello/hello';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToPage() {
+    this.navCtrl.push(HelloPage);
   }
 
 }
